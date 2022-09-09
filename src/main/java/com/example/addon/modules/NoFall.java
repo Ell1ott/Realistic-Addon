57c5d1962751838b68f5615b33c8a121fb32e3b0
Elliott <dbtracker.dk@gmail.com>
2022-09-09 15:03:47 +0200

    Update NoFall.java

--- src/main/java/com/example/addon/modules/NoFall.java
+++ src/main/java/com/example/addon/modules/NoFall.java
@@ -7,7 +7,8 @@ package com.example.addon.modules;
 
 
 import baritone.api.BaritoneAPI;
-import javassist.bytecode.analysis.ControlFlow.Block;
+import net.minecraft.block.Block;
+import meteordevelopment.meteorclient.events.entity.player.BreakBlockEvent;
 import meteordevelopment.meteorclient.events.packets.PacketEvent;
 import meteordevelopment.meteorclient.events.world.TickEvent;
 import meteordevelopment.meteorclient.mixin.PlayerMoveC2SPacketAccessor;
@@ -23,6 +24,7 @@ import meteordevelopment.meteorclient.systems.modules.Module;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 // import meteordevelopment.meteorclient.systems.modules.combat.Offhand.Item;
 import meteordevelopment.meteorclient.systems.modules.movement.Flight;
+import meteordevelopment.meteorclient.systems.modules.player.Reach;
 import meteordevelopment.meteorclient.utils.entity.EntityUtils;
 import meteordevelopment.meteorclient.utils.player.FindItemResult;
 import meteordevelopment.meteorclient.utils.player.InvUtils;
@@ -38,13 +40,18 @@ import net.minecraft.util.Hand;
 import net.minecraft.util.hit.BlockHitResult;
 import net.minecraft.util.hit.HitResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Vec3d;
+import net.minecraft.util.shape.BitSetVoxelSet;
 import net.minecraft.world.RaycastContext;
 import net.minecraft.item.Item;
 import java.io.Console;
 import java.lang.System.Logger;
+import java.util.Set;
 import java.util.function.Predicate;
 
+import net.fabricmc.loader.impl.lib.sat4j.core.Vec;
+import net.minecraft.advancement.criterion.PlayerHurtEntityCriterion;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.PowderSnowBlock;
 import net.minecraft.entity.Entity;
@@ -87,7 +94,9 @@ public class NoFall extends Module {
     private boolean placedWater;
     public Vec3d placedpos;
     private int preBaritoneFallHeight;
-    public Item[] cluchItems = {Items.LAVA_BUCKET, Items.WATER_BUCKET, Items.POWDER_SNOW_BUCKET, Items.HAY_BLOCK};
+    public Item[] cluchItems = {Items.LAVA_BUCKET, Items.WATER_BUCKET, Items.POWDER_SNOW_BUCKET, Items.HAY_BLOCK, Items.SLIME_BLOCK};
+    public static final Set<Block> cBlocks = Set.of(Blocks.SLIME_BLOCK, Blocks.POWDER_SNOW, Blocks.HAY_BLOCK);
+    // public Block[] cBlocks = {Blocks.SLIME_BLOCK, Blocks.POWDER_SNOW, Blocks.HAY_BLOCK};
     public boolean isBlock;
 
     public NoFall() {
@@ -136,7 +145,6 @@ public class NoFall extends Module {
                 return CluchItem;
             }
         }
-        mc.player.sendChatMessage("message", null);
         return CluchItem;
     }
 
@@ -165,7 +173,7 @@ public class NoFall extends Module {
 
         // Bucket mode
         if (mode.get() == Mode.MLG) {
-            if (mc.player.fallDistance > 3 && !EntityUtils.isAboveWater(mc.player)) {
+            if (mc.player.fallDistance > 3 && !EntityUtils.isAboveWater(mc.player) && !placedWater) {
                 // Place water
 
                 FindItemResult cluchItem = FindCluchItem();
@@ -179,9 +187,13 @@ public class NoFall extends Module {
                 // Check if there is a block within 5 blocks
                 BlockHitResult result = null;
 
+                Vec3d ppos = Predict(mc.player, predict.get());
+
+                Box bb = mc.player.getBoundingBox();
+
                 for (int x = 0; x < 2; x++) {
                     for (int y = 0; y < 2; y++) {
-                        mresult = mc.world.raycast(new RaycastContext(mc.player.getPos().subtract(x * 0.8 - 0.4, 0, y * 0.8 - 0.4), Predict(mc.player, predict.get()).subtract(x * 0.8 - 0.4, 1, y * 0.8 - 0.4), RaycastContext.ShapeType.OUTLINE, RaycastContext.FluidHandling.NONE, mc.player));
+                        mresult = mc.world.raycast(new RaycastContext(mc.player.getPos().subtract(x * 0.6 - 0.3, 0, y * 0.6 - 0.3), ppos.subtract(x * 0.6 - 0.3, 1, y * 0.6 - 0.3), RaycastContext.ShapeType.OUTLINE, RaycastContext.FluidHandling.NONE, mc.player));
 
 
                         if (mresult != null && mresult.getType() == HitResult.Type.BLOCK) {
@@ -189,9 +201,10 @@ public class NoFall extends Module {
                                 result = mresult;
                             }
                             else if (mresult.getBlockPos().getY() > result.getBlockPos().getY()) {
+                            }
+                            if(ppos.distanceTo(tovec3d(mresult).add(0.5, -2, 0.5)) < ppos.distanceTo(tovec3d(result).add(0.5, -2, 0.5))){
+
                                 result = mresult;
-
-
                             }
                         }
                     }
@@ -201,14 +214,14 @@ public class NoFall extends Module {
 
                 }
                 // Place water
-                if (result != null && result.getType() == HitResult.Type.BLOCK) {
+                if (result != null && result.getType() == HitResult.Type.BLOCK &&  !cBlocks.contains(mc.world.getBlockState(result.getBlockPos()).getBlock())) {
                     placedpos = tovec3d(result).add(0.5, 1, 0.5);
                     useBucket(cluchItem, true);
                 }
             }
 
             // Remove water
-            if (placedWater && mc.player.getBlockStateAtPos().getFluidState().getFluid() == Fluids.WATER || mc.player.getBlockStateAtPos().getFluidState().getFluid() == Fluids.FLOWING_WATER || mc.player.getBlockStateAtPos().getBlock() == Blocks.POWDER_SNOW || mc.player.isTouchingWater()) {
+            if (placedWater && mc.player.getBlockStateAtPos().getFluidState().getFluid() == Fluids.WATER || mc.player.getBlockStateAtPos().getFluidState().getFluid() == Fluids.FLOWING_WATER || mc.player.getBlockStateAtPos().getBlock() == Blocks.POWDER_SNOW || mc.player.isTouchingWater() || mc.player.isOnGround()) {
                 useBucket(InvUtils.findInHotbar(Items.BUCKET), false);
             }
         }
@@ -232,10 +245,11 @@ public class NoFall extends Module {
             r.getBlockPos().getZ());
     }
     private void useBucket(FindItemResult bucket, boolean placedWater) {
+        if(placedWater) mc.player.sendChatMessage("" + placedpos, null);
         if (!bucket.found()) return;
-        mc.player.sendChatMessage(String.valueOf(bucket), null);
-        if (!placedWater) isBlock=false;
 
+        if (!placedWater) isBlock=false;
+        if(PlayerUtils.distanceTo(placedpos) > 5) return;
 
         Rotations.rotate(Rotations.getYaw(placedpos), Rotations.getPitch(placedpos), 100, true, () -> {
             if (bucket.isOffhand()) {
@@ -243,9 +257,9 @@ public class NoFall extends Module {
             } else {
                 // int preSlot = mc.player.getInventory().selectedSlot;
                 InvUtils.swap(bucket.slot(), true);
-                if(isBlock) BlockUtils.place(new BlockPos(placedpos), bucket, true, 0);
+                if(isBlock)  BlockUtils.place(new BlockPos(placedpos), bucket, true, 0);
                 else mc.interactionManager.interactItem(mc.player, Hand.MAIN_HAND);
-                // InvUtils.swapBack();
+                InvUtils.swapBack();
             }
 
             this.placedWater = placedWater;
