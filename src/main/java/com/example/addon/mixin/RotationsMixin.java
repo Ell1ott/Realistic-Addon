e86ad7d3afb59f90b07376f57e56a0190427aee0
Elliott <dbtracker.dk@gmail.com>
2022-09-20 15:16:52 +0200

    Create RotationsMixin.java

---
+++ src/main/java/com/example/addon/mixin/RotationsMixin.java
@@ -0,0 +1,30 @@
+package com.example.addon.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+import com.example.addon.Utils.Logger;
+
+import meteordevelopment.meteorclient.utils.player.Rotations;
+import com.example.addon.Utils.ConfigModifier;
+import meteordevelopment.meteorclient.utils.player.Rotations;
+
+@Mixin(Rotations.class)
+public abstract class RotationsMixin {
+
+    @Shadow
+    Rotation r;
+    // @Overwrite
+    // public void rotate(double yaw, double pitch, int priority, boolean clientSide, Runnable callback) {
+    //     Logger.Log("h");
+    // }
+    @Overwrite
+    public static void rotate(double yaw, double pitch, int priority, boolean clientSide, Runnable callback) {
+        Logger.Log("h"+ConfigModifier.get().predict.get());
+
+    }
+
+
+}
