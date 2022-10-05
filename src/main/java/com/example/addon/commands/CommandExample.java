78c1cad12b974d00ca322e76b26520883be6c7d8
Elliott <dbtracker.dk@gmail.com>
2022-10-05 13:46:35 +0200

    Update CommandExample.java

--- src/main/java/com/example/addon/commands/CommandExample.java
+++ src/main/java/com/example/addon/commands/CommandExample.java
@@ -1,7 +1,11 @@
 package com.example.addon.commands;
 
 import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+
+import meteordevelopment.meteorclient.events.world.TickEvent;
 import meteordevelopment.meteorclient.systems.commands.Command;
+import meteordevelopment.meteorclient.utils.player.InvUtils;
+import meteordevelopment.orbit.EventHandler;
 import net.minecraft.command.CommandSource;
 
 import static com.mojang.brigadier.Command.SINGLE_SUCCESS;
@@ -15,7 +19,29 @@ public class CommandExample extends Command {
     public void build(LiteralArgumentBuilder<CommandSource> builder) {
         builder.executes(context -> {
             info("hi");
+            mc.player.sendCommand("warp end", null);
+            InvUtils.drop().slotHotbar(0);
             return SINGLE_SUCCESS;
         });
+    }
+
+    // @Override
+    // public void build(LiteralArgumentBuilder<CommandSource> builder) {
+    //     builder.then(argument("damage", IntegerArgumentType.integer(1, 7)).executes(context -> {
+    //         int amount = IntegerArgumentType.getInteger(context, "damage");
+
+    //         if (mc.player.getAbilities().invulnerable) {
+    //             throw INVULNERABLE.create();
+    //         }
+
+    //         damagePlayer(amount);
+    //         return SINGLE_SUCCESS;
+    //     }));
+
+
+
+    @EventHandler
+    private void onTick(TickEvent.Pre event) {
+        info("hi");
     }
 }
